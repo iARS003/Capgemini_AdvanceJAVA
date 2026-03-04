5855ffdf6e4eb6973d76db73cabd0521b539121a
iARS003 <142662459+iARS003@users.noreply.github.com>
2026-03-05 00:09:57 +0530

    Delete Tasks.java

--- Tasks.java
+++
@@ -1,8 +0,0 @@
-import java.sql.*;
-
-public class DatabaseConfig {
-public static Connection getConnection()throws SQLException, ClassNotFoundException{
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    return DriverManager.getConnection("jdbc:mysql://localhost:3306/tasks","root","root");
-    }
-}
